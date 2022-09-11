f30252fb52a94836b666f52b11b0ccee7638a451
Jawid <ajawid@auaf.edu.af>
2022-09-11 14:00:11 +0430

    push11Sep

--- statDataAnalysisFall22.R
+++ statDataAnalysisFall22.R
@@ -1,24 +1,73 @@
 ####+++++++++++++++Fall 2022/ITC 255++++++++++++#
-x=-1
-y=12
+#install R/Rstudio
+#connect to git/github
 
-x+y
 
-p=x*y
-p
-
-#data types in R 
-
-a=FALSE
-class(a)
-class(p)
-b=-1.2
-class(b)
-#data types in R 
-
-x=c(1,2,3)
+##Introduction to R
+#1. Data types
+x=12L
 class(x)
-
-##Data types in R
-y='text'
+y=-12.2
 class(y)
+t='R progmming'
+class(t)
+p=2<3
+p
+class(p)
+typeof(t)
+#2. Data structures in R 
+#vector
+x=c(1,2,-1)
+typeof(x)
+p=c(TRUE, FALSE)
+typeof(p)
+a=c('F','M')
+typeof(a)
+class(a)
+length(a)
+str(a)
+str(x)
+
+x[1]
+a[2]
+
+#List
+l=list('F', 'M', c(22,24), c('yes', 'no', 12))
+l
+typeof(l)
+class(l)
+length(l)
+str(l)
+
+#Matrix..2D
+
+m=matrix(c(1,2,3,4), nrow = 2, ncol = 2, byrow = T)
+m
+
+typeof(m)
+class(m)
+str(m)
+length(m)
+dim(m)
+m[1,]
+m[,1]
+m[2,2]
+
+#dataframe 2-d
+
+df=data.frame(gender=c('f', 'm'),
+              age=c(23, 34),
+              mstatus=c('married', 'married'), 
+              height=c(161.2, 188.8))
+df
+
+typeof(df)
+class(df)
+length(df)  #number of columns
+str(df)
+
+df[1,]
+df[,2]
+
+###3. 
+
